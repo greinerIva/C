aaacaa0ecadba361608e22faa5dc491b264a01aa
greinerIva <122392512+greinerIva@users.noreply.github.com>
2023-02-04 15:02:48 +0300

    Add files via upload

---
+++ XDGame.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Do you want play a game?\n");
+	printf("\nY - yes N - no\n");
+	int 
+	awnser = getchar();
+	int a = 0;
+	if (awnser == 89 )
+	{
+		printf("\nBut game doesn't want");
+	}
+	if (awnser == 121)
+	{
+		printf("\nBut game doesn't want");
+	}
+	if (awnser == 78)
+	{
+		printf("\nBut game wants");
+		a = 1;
+	}
+	if (awnser == 110)
+	{
+		printf("\nBut game wants");
+		a = 1;
+	}
+	if (a == 1)
+	{
+		awnser = 0;
+		printf("\n2+2=\n");
+		while (a = 1)
+		{	
+			
+			if (awnser == 52)
+			{
+				printf("\nCorrect");
+				a = 0;
+				break;
+			}
+			 else if(awnser == 53)
+			{
+				printf("\nGenious");
+				a = 0;
+				break;
+			}
+			
+		}
+	}
+}
